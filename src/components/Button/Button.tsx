97f111e9eb13146b6ef4e3a45b5974b63b59c039
navinnavneetag <navin.navneet@agnext.in>
2023-07-27 11:40:12 +0530

    trying to set up chakra-ui

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+const ButtonChakra = React.lazy(() => import("@chakra-ui/react").then((module) => ({ default: module.Button })));
+import "./Button.css";
 
 interface ButtonProps {
   label: string;
+  children: any;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <ButtonChakra colorScheme='teal' size="lg" onClick={props.onClick}>
+      {props.label} {props.children}
+    </ButtonChakra>
+  );
 };
 
 export default Button;
+
+/*
+import React from 'react';
+
+// Lazy import Chakra UI components
+const Button = React.lazy(() => import("@chakra-ui/react").then((module) => ({ default: module.Button })));
+const Input = React.lazy(() => import("@chakra-ui/react").then((module) => ({ default: module.Input })));
+
+const MyComponent = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+      <Input placeholder="Enter something" />
+    </div>
+  );
+};
+
+export default MyComponent;
+*/
